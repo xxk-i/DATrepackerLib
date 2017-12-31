2c3dcfaf189314daa70b2bc478fd7caf92dbbbc8
xxk-i <29494813+xxk-i@users.noreply.github.com>
2017-12-31 16:20:14 -0500

    Add files via upload

---
+++ src/FilePackInfo.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+// Lol what is c#
+namespace DATRepackerLib
+{
+    public class FilePackInfo
+    {
+        private string PackDirectory;
+
+        internal int FileCount = 0;
+        internal List<string> QualifiedFileNames = new List<string>();
+        internal List<string> FileBaseNames = new List<string>();
+        internal List<string> FileExtensions;
+
+        internal MetadataTable FileOffsetTable;
+        internal MetadataTable ExtensionTable;
+        internal MetadataTable NameTable;
+        // Length of longest name + 1
+        internal int NameTableBlockSize;
+        internal MetadataTable SizeTable;
+        internal MetadataTable CrcTable;
+        internal Dictionary<string, long> FileSizeDict; 
+
+        public FilePackInfo(string inDirectory)
+        {
+            PackDirectory = inDirectory;
+            QualifiedFileNames = new List<string>(Directory.GetFiles(PackDirectory));
+            FileBaseNames = GetFileBaseNames();
+            FileCount = QualifiedFileNames.Count;
+
+            FileExtensions = GetFileExtensions();
+            NameTableBlockSize = GetNameTableBlockSize();
+            FileSizeDict = GetFileSizeDict();
+
+            FileOffsetTable = new MetadataTable
+            {
+                Size = 4 * FileCount,
+                Offset = 32  // Header is fixed at 32 bytes
+            };
+
+            ExtensionTable = new MetadataTable
+            {
+                Size = 4 * FileCount,
+                Offset = FileOffsetTable.EndOffset()
+            };
+
+            NameTable = new MetadataTable
+            {
+                // 4 bytes for blocksize, then one block per file
+                Size = 4 + NameTableBlockSize * FileCount,
+                Offset = ExtensionTable.EndOffset(),
+            };
+            NameTable.EndPadding = NameTable.EndOffset() % 4 == 0 
+                    ? 0 
+                    : 4 - (NameTable.EndOffset() % 4);
+
+            SizeTable = new MetadataTable
+            {
+                Size = 4 * FileCount,
+                Offset = NameTable.EndOffset()
+            };
+
+            // I'm just faking crc table as 16 NUL bytes and then padded to 
+            // 16 total bytes
+            CrcTable = new MetadataTable
+            {
+                Size = 16,
+                Offset = SizeTable.EndOffset(),
+            };
+            CrcTable.EndPadding = CrcTable.EndOffset() % 16 == 0 
+                    ? 0
+                    : 16 - (CrcTable.EndOffset() % 16);
+        }
+
+        private List<string> GetFileExtensions() 
+        {
+            List<string> extensions = new List<string>(FileCount);
+            foreach (string file in QualifiedFileNames) 
+            {
+                string path = Path.GetExtension(file);
+                path = path.TrimStart('.');
+                extensions.Add(path);
+            }
+
+            return extensions;
+        }
+
+        /// <summary>
+        /// Block size is the longest file name + 1 for NUL byte
+        /// </summary>
+        /// <returns>Blocksize</returns>
+        private int GetNameTableBlockSize()
+        {
+            int size = 0;
+            foreach (string file in FileBaseNames) 
+            {
+                if (file.Length > size) 
+                {
+                    size = file.Length;
+                }
+            }
+
+            return size + 1;
+        }
+
+        private List<string> GetFileBaseNames()
+        {
+            List<string> basenames = new List<string>(FileCount);
+            foreach (string file in QualifiedFileNames)
+            {
+                FileInfo fi = new FileInfo(file);
+                basenames.Add(fi.Name);
+            }
+
+            return basenames;
+        }
+
+        private Dictionary<string, long> GetFileSizeDict() 
+        {
+            Dictionary<string, long> d = new Dictionary<string, long>();
+            foreach (string file in QualifiedFileNames) 
+            {
+                d.Add(file, (new FileInfo(file)).Length);
+            }
+
+            return d;
+        }
+
+        internal List<int> GetHeaderTableOffsets() 
+        {
+            return new List<int>
+            {
+                FileOffsetTable.Offset,
+                ExtensionTable.Offset,
+                NameTable.Offset,
+                SizeTable.Offset,
+                CrcTable.Offset
+            };
+        }
+
+        internal struct MetadataTable
+        {
+            // All in bytes
+            internal int Offset;
+            internal int Size;
+            internal int EndPadding;
+
+            internal int EndOffset()
+            {
+                return Offset + Size + EndPadding;
+            }
+
+            internal int TotalSize()
+            {
+                return Size + EndPadding;
+            }
+        }
+
+    }
+}
---
+++ src/Writer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DATRepackerLib
+{
+    public class Writer
+    {
+        private static readonly byte[] MAGIC = { 0x44, 0x41, 0x54, 0x00 };
+        private static readonly byte[] NUL = { 0x00 };
+        private readonly FilePackInfo PackInfo;
+
+        public Writer(FilePackInfo packInfo) 
+        {
+            PackInfo = packInfo;
+        }
+
+        public void WriteToFile(string outFile)
+        {
+            if (PackInfo == null)
+            {
+                throw new InvalidOperationException(
+                        "Error: need valid FilePackInfo to write file");
+            }
+
+            try 
+            {
+                using (var fs = new FileStream(
+                        outFile, FileMode.Create, FileAccess.Write))
+                {
+                    WriteFile(fs);
+                }
+            }
+            catch (Exception ex) 
+            {
+                throw ex;
+            }
+
+        }
+
+        // pad int to 4 bytes, little endian
+        private byte[] PaddedBytes(int num)
+        {
+            byte[] bytes = BitConverter.GetBytes(num);
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+
+            return bytes;
+        }
+
+        private static void _write(FileStream stream, byte[] bytes)
+        {
+            //FIXME: this might break?
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        private static int _calculate16BytePadding(int offset)
+        {
+            int overflow = offset % 16;
+            if (overflow == 0)
+            {
+                return 0;
+            }
+
+            return 16 - overflow;
+        }
+
+        private static byte[] NulPad(int length)
+        {
+            byte[] pad = new byte[length];
+            return pad;
+        }
+
+        // Go to town
+        private void WriteFile(FileStream stream)
+        {
+            // for brevity
+            FilePackInfo pi = PackInfo;
+
+            ///////////
+            // First 32 bytes
+            ///////////
+
+            stream.Write(MAGIC, 0, 4);
+            _write(stream, PaddedBytes(pi.FileCount));
+            // There are 5 tables
+            foreach (int offset in pi.GetHeaderTableOffsets()) 
+            {
+                _write(stream, PaddedBytes(offset));
+            }
+
+            // 4 bytes pads out to 32
+            _write(stream, PaddedBytes(0x00));
+
+            ///////////
+            // end of offset data
+            ///////////
+
+            // File offset table
+            int fileOffset = pi.CrcTable.EndOffset();
+            foreach (string file in pi.QualifiedFileNames)
+            {
+                // eh
+                int size = (int) pi.FileSizeDict[file];
+                _write(stream, PaddedBytes(fileOffset));
+                fileOffset += size;
+                fileOffset += _calculate16BytePadding(fileOffset);
+            }
+
+            // Extension table
+            foreach (string ext in pi.FileExtensions)
+            {
+                _write(stream, Encoding.ASCII.GetBytes(ext));
+                _write(stream, NUL);
+            }
+
+            // Name table
+            _write(stream, PaddedBytes(pi.NameTableBlockSize));
+            foreach (string file in pi.FileBaseNames)
+            {
+                // Not little-endian
+                int padLength = pi.NameTableBlockSize - file.Length;
+                _write(stream, Encoding.ASCII.GetBytes(file));
+                _write(stream, NulPad(padLength));
+            }
+            // make sure to fall on a 4-byte boundary
+            int overflow = (int) stream.Position % 4;
+            if (overflow != 0)
+            {
+                _write(stream, NulPad(4 - overflow));
+            }
+
+            // Size table
+            foreach (string file in pi.QualifiedFileNames)
+            {
+                _write(stream, PaddedBytes((int) pi.FileSizeDict[file]));
+            }
+
+            // CRC table (just nuls)
+            _write(stream, NulPad(pi.CrcTable.TotalSize()));
+
+
+            // Now just glom on files
+            foreach (string file in pi.QualifiedFileNames)
+            {
+                // Files have to start on 16 byte boundaries, therefore
+                // the offset % 16 will always be zero and can be ignored
+                int size = (int) pi.FileSizeDict[file];
+                int underflow = _calculate16BytePadding(size);
+
+                // Open the file and write it
+                byte[] fileBytes = File.ReadAllBytes(file);
+                _write(stream, fileBytes);
+                if (underflow > 0)
+                {
+                    _write(stream, NulPad(underflow));
+                }
+            }
+        }
+    }
+}
